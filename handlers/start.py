858ae424f7f2e6c0df0ac10216a3faeffd866f5b
Dhimas Azman <85933622+dhimasazman@users.noreply.github.com>
2022-06-09 01:49:38 +0700

    small update

--- handlers/start.py
+++ handlers/start.py
@@ -11,33 +11,37 @@ async def start_(client: Client, message: Message):
     await message.delete()
     await message.reply_photo(
         photo=f"{START_IMG}",
-        caption=f"""**━━━━━━━━━━━━━━━━━━
- Hai {message.from_user.mention()} !
+        caption=f""" Hai {message.from_user.mention()} 👋
 
-        Saya adalah [{bn}](t.me/{bu}), bot yang bisa streaming di OS/VCG !
+Saya adalah [{bn}](t.me/{bu}), bot yang bisa streaming musik di OS/VCG !
+jangan sungkan tambahkan saya ke grup anda ya...
 
-Semua perintah , kalian bisa cek perintah berikut : ( `/ . • $ ^ ~ + * ?` )
-Jika kalian punya pertanyaan atau masalah lainnya kalian bisa PM [Owner](t.me/erojistrix)...
-━━━━━━━━━━━━━━━━━━**""",
+Semua perintah , kalian bisa pakai simbol berikut : ( `/ . • $ ^ ~ + * ?` )
+
+Jika kalian punya pertanyaan atau masalah lainnya kalian bisa hubungi [Owner](t.me/erojistrix)
+
+**Jika kalian ingi donasi supaya projek ini terus berkembang kalian boleh donasi [Disini](https://bit.ly/donasidhimasazman)**
+
+[Channel](t.me/AzumanProjects) • [Grup](t.me/AzumanSquad)""",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "➕ Tambahkan saya ke grup anda", url=f"https://t.me/{bu}?startgroup=true"
+                        "➕ Tambahkan saya ke grup anda ➕", url=f"https://t.me/{bu}?startgroup=true"
                        ),
                   ],[
                     InlineKeyboardButton(
-                        "Developer", url=f"https://t.me/erojistrix"
+                        "👤 Developer", url=f"https://t.me/erojistrix"
                     ),
                     InlineKeyboardButton(
-                        "Support", url=f"https://t.me/{SUPPORT_GROUP}"
+                        "📥 Support", url=f"https://t.me/{SUPPORT_GROUP}"
                     )
                 ],[
                     InlineKeyboardButton(
                         "Inline", switch_inline_query_current_chat=""
                     ),
                     InlineKeyboardButton(
-                        "Youtube", url="https://youtube.com/channel/UCtI7hbY-BD7wvuIzoSU0cEw"
+                        "📺 Youtube", url="https://youtube.com/channel/dhimasazman"
                     )]
             ]
        ),
