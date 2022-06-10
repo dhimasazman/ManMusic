b2b2dbf4d4502b7a7b6ec852b848b4bcd3eed022
Dhimas Azman <85933622+dhimasazman@users.noreply.github.com>
2022-06-10 12:50:49 +0700

    update

--- handlers/song.py
+++ handlers/song.py
@@ -49,13 +49,13 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit(f"📂 Mengunduh lagu dari server YouTube...")
+    m.edit(f"Sedang mengunduhh mohon tunggu...")
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"**Hasil pengunduhan**\n\n👤Diminta oleh :{chutiya}\n🔍Hasil penelusuruan : {query}\n\n⚡𝘗𝘰𝘸𝘦𝘳𝘦𝘥 𝘣𝘺 [{bn}](t.me/{bu})"
+        rep = f"❇️**Detail Pengunduhan :**\n\n🔍**Hasil penelusuruan** :\n👤**Diminta oleh** :{chutiya} {query}\n\n⚡𝘗𝘰𝘸𝘦𝘳𝘦𝘥 𝘣𝘺 [{bn}](t.me/{bu})"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
