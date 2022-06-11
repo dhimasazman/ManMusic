a66ef4443185fdc18ab7faf17eb23e08464c47f4
Dhimas Azman <85933622+dhimasazman@users.noreply.github.com>
2022-06-11 17:51:17 +0700

    Update id.py

--- handlers/id.py
+++ handlers/id.py
@@ -22,7 +22,7 @@ async def showid(_, message: Message):
         _id += "<b>➥ ID Chat grup :</b>" f"<code>{message.chat.id}</code>\n"
         if message.reply_to_message:
             _id += (
-                "<b>➥ ID Akun user :</b>:"
+                "<b>➥ ID Akun user :</b>"
                 f"<code>{message.reply_to_message.from_user.id}</code>\n"
             )
             file_info = get_file_id(message.reply_to_message)
